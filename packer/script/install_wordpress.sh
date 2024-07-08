1fd4ce25e8c84ac2b30458ee46a46320face7588
Pavlo <pavel_petrov@ukr.net>
2024-07-08 11:17:04 +0300

    add enviroment in packer

--- packer/script/install_wordpress.sh
+++ packer/script/install_wordpress.sh
@@ -80,3 +80,4 @@ else
   sudo -u www-data wp core install --url="${WP_URL}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}" --path=/var/www/html
   echo "WordPress успішно встановлено!"
 fi
+
