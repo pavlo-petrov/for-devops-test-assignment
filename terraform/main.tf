a759d11d979fb2ae7e1eb7770bbf02be56d8bf22
Pavlo <pavel_petrov@ukr.net>
2024-07-15 10:28:57 +0300

    some changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -195,22 +195,20 @@ resource "aws_elasticache_cluster" "redis_cluster" {
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis7"
-  engine_version       = "7.1"
+  parameter_group_name = "default.redis6.x"
+  engine_version       = "6.0"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
   security_group_ids   = [aws_security_group.db.id]
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes  = [
-      node_type,
-      number_cache_clusters,
-      parameter_group_name,
-      engine_version,
-  ]
-}
-
+  # lifecycle {
+  #   prevent_destroy = true
+  #   ignore_changes  = [
+  #     node_type,
+  #     number_cache_clusters,
+  #     parameter_group_name,
+  #     engine_version,
+  # ]
+  #}
 }
 
 ##################### security group for parcker #################
