4e03003928b60c2e6860fd453efee600b4524562
Pavlo <pavel_petrov@ukr.net>
2024-07-16 14:54:24 +0300

    add some with fixxxx3 with hotfix

--- packer/script/install_wordpress.sh
+++ packer/script/install_wordpress.sh
@@ -124,7 +124,7 @@ mysql --defaults-extra-file=~/.my.cnf --silent --skip-column-names --batch -e "F
 echo "!!!!!!user added or exist!!!!!!"
 
 # Створення файлу wp-config.php
-sudo -u www-data cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+# sudo -u www-data cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 sed -i "s/database_name_here/${DB_NAME}/" /var/www/html/wp-config.php
 sed -i "s/username_here/${WP_ADMIN_USER}/" /var/www/html/wp-config.php
 sed -i "s/password_here/${WP_ADMIN_PASSWORD}/" /var/www/html/wp-config.php
