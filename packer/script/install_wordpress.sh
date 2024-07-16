c6f961d10969dc862c53372905ca4abdf2a8df8c
Pavlo <pavel_petrov@ukr.net>
2024-07-16 18:31:46 +0300

    test

--- packer/script/install_wordpress.sh
+++ packer/script/install_wordpress.sh
@@ -126,6 +126,8 @@ MY_S3=$AWS_S3_WORDPRESS_NAME_S3
 
 # Створення файлу wp-config.php
 # sudo -u www-data cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+echo "!!!!!!setup started!!!!!!"
+
 sed -i "s/database_name_here/${DB_NAME}/" /var/www/html/wp-config.php
 sed -i "s/username_here/${WP_ADMIN_USER}/" /var/www/html/wp-config.php
 sed -i "s/password_here/${WP_ADMIN_PASSWORD}/" /var/www/html/wp-config.php
